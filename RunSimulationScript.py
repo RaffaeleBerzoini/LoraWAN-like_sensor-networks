a20343bdedf72225dea6930341e6957080dccb8e
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2023-08-08 12:43:42 +0200

    minor changes

--- RunSimulationScript.py
+++ RunSimulationScript.py
@@ -99,7 +99,7 @@ for i in range(1, 9):
 
 print "Start simulation with TOSSIM! \n\n\n";
 
-for i in range(0,4500):
+for i in range(0,45000):
 	t.runNextEvent()
 	
 print "\n\n\nSimulation finished!";
